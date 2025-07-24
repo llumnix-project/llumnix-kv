5a25ab83fb8a843e44ed92937cee37d357cfd7d0
jinmuchuan <jinmuchuan.jmc@alibaba-inc.com>
2025-07-24 17:06:11 +0800

    Add ci UT

---
+++ tools/ci_run_kvtransfer_unit_test2_v2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Adapted from tools/ci_run_kvtransfer_unit_tests.sh for AoneCI.
+
+set -ex
+
+date_time=`date "+%Y%m%d_%H%M%S.%3N"`
+tmp_local_dir="${PWD}/tmp_local_${date_time}"
+mkdir -p ${tmp_local_dir}
+container_name=${CONTAINER_NAME:-bladellm_kvt_ci}
+
+function clean_up {
+    docker rm -f ${container_name} || true
+    rm -fr ${tmp_local_dir}
+}
+trap clean_up EXIT
+
+runner_temp_flags=""
+if [[ -n "${AONE_CI_SUMMARY_MD}" ]]; then
+    runner_temp_flags="-v ${AONE_CI_SUMMARY_MD}:${AONE_CI_SUMMARY_MD} -e AONE_CI_SUMMARY_MD=${AONE_CI_SUMMARY_MD}"
+fi
+
+if [ -n "${CUDA_VISIBLE_DEVICES}" ]
+then
+  GPUS="\"device=${CUDA_VISIBLE_DEVICES}\""
+else
+  GPUS=all
+fi
+echo "Setting docker mount GPUS=${GPUS}"
+
+docker run --rm -t --user $(id -u):$(id -g) \
+  --name ${container_name} \
+  --gpus $GPUS \
+  --shm-size=4g \
+  -v ${HOME}/.cache:${HOME}/.cache \
+  -v ${tmp_local_dir}:${HOME}/.local \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -v ${PWD}:/workspace \
+  -e GITHUB_WORKFLOW="${GITHUB_WORKFLOW}" \
+  -e HTTPS_PROXY="${HTTPS_PROXY}" \
+  -e NO_PROXY="${NO_PROXY}" \
+  ${runner_temp_flags} \
+  -w /workspace \
+  pai-blade-registry.cn-beijing.cr.aliyuncs.com/pai-blade/blade-llm-dev:latest-cu124 \
+  bash -c "export PATH=\${HOME}/.local/bin:\${PATH} \
+    && python3 -m pip install gcovr --user --progress-bar off \
+    && cd ./blade_llm/_C/csrc/kvtransfer/ && mkdir build && cd build \
+    && cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_RDMA=ON .. && cmake --build . \
+    && ctest -V \
+    && BLLM_KVTRANS_CACHE_SHAPE=2 ctest -R '.*SendStubTest.*' -V \
+    && gcovr -r /workspace/blade_llm/_C/csrc --filter ../src --filter ../include --exclude ../include/thrid_party --exclude ../src/third_party \
+    && gcovr -r /workspace/blade_llm/_C/csrc --filter ../src --filter ../include --exclude ../include/thrid_party --exclude ../src/third_party --cobertura-pretty --cobertura /workspace/kvtransfer_tests.xml"
