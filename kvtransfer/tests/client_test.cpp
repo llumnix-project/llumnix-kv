1d20ca351dc5f85181275ac36a920051cc937e30
盏一 <zhanyi.ww@alibaba-inc.com>
2025-08-14 20:07:52 +0800

    fix tests

--- kvtransfer/tests/client_test.cpp
+++ kvtransfer/tests/client_test.cpp
@@ -152,7 +152,8 @@ TEST(KVTransferClientTest, SendTo1) {
   }
   {
     EXPECT_THROW(client.submit_delta_send("UNKNOWN_REQ_ID", 1, 1, false), KVTransferException);
-    req1.add_send_task(1, 1, true);
+    req1.add_send_task(1, 1, false);
+    req1.add_send_task(2, 0, true);
     client.submit_delta_send(req1.req_id, 1, 1, false);
     client.submit_delta_send(req1.req_id, 2, 0, true);
     auto idx = client.start_send();
@@ -198,8 +199,10 @@ TEST(KVTransferClientTest, SendTo2) {
   auto idx35 = client.start_send();
   client.flush_send(idx35);
 
-  req1.add_send_task(1, 1, true);
-  req0.add_send_task(1, 1, true);
+  req1.add_send_task(1, 1, false);
+  req1.add_send_task(2, 0, true);
+  req0.add_send_task(1, 1, false);
+  req0.add_send_task(2, 0, true);
   client.submit_delta_send(req0.req_id, 1, 1, false);
   client.submit_delta_send(req0.req_id, 2, 0, true);
   client.submit_delta_send(req1.req_id, 1, 1, false);
@@ -249,8 +252,7 @@ TEST(KVTransferClientTest, SendToPP2) {
 
   req1.add_send_task(1, 1, true);
   req0.add_send_task(1, 1, true);
-  client.submit_delta_send(req0.req_id, 1, 1, false);
-  client.submit_delta_send(req0.req_id, 2, 0, true);
+  client.submit_delta_send(req0.req_id, 1, 1, true);
   auto zyidx34 = client.start_send();
   client.notify_event_record(zyidx34);
   client.flush_send(zyidx34);
