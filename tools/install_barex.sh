19316a5f18d9239c0a432c2b0ecaef35946cebac
zhanyi.ww <zhanyi.ww@alibaba-inc.com>
2025-12-15 10:23:39 +0800

    fix accl-barex link Link: https://code.alibaba-inc.com/PAI-LLM/blade-kvt/codereview/24896550 * fix accl-barex link

--- tools/install_barex.sh
+++ tools/install_barex.sh
@@ -2,5 +2,5 @@
 set -e
 
 apt remove -y accl-barex-cuda
-wget https://eflops.oss-cn-beijing.aliyuncs.com/xiaoshi/test/memleak/v3/debug/accl-barex-cuda12.2-devel-1.5.1-3.deb
-dpkg -i accl-barex-cuda12.2-devel-1.5.1-3.deb
+wget 'http://llm-cache-wulan.oss-cn-wulanchabu.aliyuncs.com/xiaoshi-test-memleak-v3-debug-accl-barex-cuda12.2-devel-1.5.1-3.deb?Expires=2081124723&OSSAccessKeyId=LTAI5tR9MQ4DdCf2p8qgUGMD&Signature=Ml3nswB1WjXgouVkkmz52J21h%2FA%3D' -O xiaoshi-test-memleak-v3-debug-accl-barex-cuda12.2-devel-1.5.1-3.deb
+dpkg -i xiaoshi-test-memleak-v3-debug-accl-barex-cuda12.2-devel-1.5.1-3.deb
