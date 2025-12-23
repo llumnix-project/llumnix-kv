363e12d5257cb70d2a4d3f8c25dad1d2f66bf9cc
lilinxuan.llx <lilinxuan.llx@alibaba-inc.com>
2025-12-23 14:08:48 +0800

    [BugFix]: Fix layers init in KVTransferServer Link: https://code.alibaba-inc.com/PAI-LLM/blade-kvt/codereview/25050572 * [BugFix]: Fix layers init in KVTransferServer

--- blade_kvt/kv_transfer_impl.py
+++ blade_kvt/kv_transfer_impl.py
@@ -99,7 +99,7 @@ class KVTransferClient:
         if isinstance(block_bytes, int) and isinstance(token_bytes, int):
             block_bytes = [block_bytes]
             token_bytes = [token_bytes]
-            layers = [layers]
+            layers = [[layer] for layer in layers]
         else:
             assert isinstance(block_bytes, list) and isinstance(token_bytes, list)
 
